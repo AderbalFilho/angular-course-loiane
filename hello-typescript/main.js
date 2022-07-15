b0aba7029297a4d9541040028dce8792a84bbddb
AderbalFilho <aderbalcrato@yahoo.com.br>
2022-07-15 15:16:49 +0100

    class 4

---
+++ hello-typescript/main.js
@@ -0,0 +1,23 @@
+var myVar = "minha variável";
+function myFunc(x, y) {
+  return x + y;
+}
+// ES6 ou ES 2015
+var num = 2;
+var PI = 3.14;
+var numbers = [1, 2, 3];
+numbers.map(function (value) {
+  return value * 2;
+});
+numbers.map(function (value) {
+  return value * 2;
+}); // ES 2015
+var Mathematics = function () {
+  function Mathematics() {}
+  Mathematics.prototype.sum = function (x, y) {
+    return x + y;
+  };
+  return Mathematics;
+};
+var n1 = "sdfsdf";
+n1 = 4;
---
+++ hello-typescript/main.ts
@@ -0,0 +1,24 @@
+var myVar = 'minha variável';
+
+function myFunc(x, y) {
+  return x + y;
+}
+
+// ES6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numbers = [1, 2, 3];
+numbers.map(function(value) {
+  return value * 2;
+});
+numbers.map((value) => value * 2); // ES 2015
+
+class Mathematics {
+  sum(x, y) {
+    return x + y;
+  };
+}
+
+var n1 = 'sdfsdf';
+n1 = 4;
